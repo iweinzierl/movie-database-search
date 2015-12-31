282df54d06f651382c6ed1d35a5b14459f73e5ac
Ingo Weinzierl <weinzierl.ingo@gmail.com>
2015-12-31 14:38:38 +0100

    Disabled csrf security.

--- src/main/java/com/github/iweinzierl/moviedatabase/search/config/SecurityConfig.java
+++ src/main/java/com/github/iweinzierl/moviedatabase/search/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.github.iweinzierl.moviedatabase.search.config;
 import com.github.iweinzierl.moviedatabase.search.config.security.UserDetailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -15,5 +16,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
     }
 }
